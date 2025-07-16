afd4bc616760f977cf0cd74f7d220751895607a4
Ivan de Oliveira <ivan.oliveira.pe@outlook.com>
2025-07-16 19:57:05 -0300

    Adicionado eventos via JS e realizado ajustes de responsividade

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const dt = document.querySelectorAll(".container-dt");
+const dd = document.querySelectorAll("dd");
+const minusIcon = document.querySelectorAll(".icon-minus");
+const plusIcon = document.querySelectorAll(".icon-plus");
+
+window.onload = function () {
+  dd[0].classList.add("show");
+  minusIcon[0].style.display = "block";
+  plusIcon[0].style.display = "none";
+};
+
+dt.forEach((element) => {
+  element.addEventListener("click", function () {
+    element.nextElementSibling.classList.toggle("show");
+    const dd = this.nextElementSibling;
+    const minusIcon = this.querySelector(".icon-minus");
+    const plusIcon = this.querySelector(".icon-plus");
+
+    if (dd.classList == "show") {
+      console.log("teste");
+      plusIcon.style.display = "none";
+      minusIcon.style.display = "block";
+    } else {
+      minusIcon.style.display = "none";
+      plusIcon.style.display = "block";
+    }
+  });
+});
